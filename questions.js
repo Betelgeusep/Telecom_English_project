47b22fafdb4dbf0f71cd52a82f948bcf5058ad44
Betelgeusep <pmedrano566@gmail.com>
2023-07-28 11:01:15 -0400

    se agrega el proyecto

---
+++ questions.js
@@ -0,0 +1,63 @@
+let questions=[
+    {
+        numb:1,
+        question:"A communications medium consisting of a pair of wires?",
+        answer:"C. Twisted Pair",
+        options:[
+            "A. Terrestrial Microwave",
+            "B. Coaxial Cable",
+            "C. Twisted Pair",
+            "D. Fiber Optics"
+
+        ]
+    },
+    {
+        numb:2,
+        question:"What does microwaves mean in spanish?",
+        answer:"A. Microondas",
+        options:[
+            "A. Microondas",
+            "B. Ondas micro",
+            "C. Microseñales",
+            "D. Microbios"
+
+        ]
+    },
+    {
+        numb:3,
+        question:"How can I say in English 'inalámbrico'? ",
+        answer:"A. Wireless",
+        options:[
+            "A. Wireless",
+            "B. Transmission",
+            "C. Microwaves",
+            "D. Radiowaves"
+
+        ]
+    },
+    {
+        numb:4,
+        question:"What is the media by which the signal is transmitted from an earth station to a satellite?",
+        answer:"D. Satellite Transmissions",
+        options:[
+            "A. Twisted Pair",
+            "B. Fiber Optics",
+            "C. Terrestrial Microwave",
+            "D. Satellite Transmissions"    
+
+        ]
+    },
+    {
+        numb:5,
+        question:"What does 'shielded' mean in spanish?",
+        answer:"D. Blindado",
+        options:[
+            "A. Escudado",
+            "B. Nucleo",
+            "C. Impermeable",
+            "D. Blindado"
+
+        ]
+    },
+
+];
